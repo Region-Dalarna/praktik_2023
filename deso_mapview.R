61a2fd74c536104806b68050c0d1498e3dcf865e
hen-ald <henrik.alden@regiondalarna.se>
2023-10-23 18:43:59 +0200

    sparade deso som dalarna

--- deso_mapview.R
+++ deso_mapview.R
@@ -1,7 +1,14 @@
 library(sf)
 library(mapview)
+library(dplyr)
 
 deso_fil <- "DeSo_2018_v2.gpkg"
-deso <- st_read(deso_fil, crs = 3006)
+
+deso <- st_read(deso_fil, crs = 3006) %>% 
+  filter(lan == '20')
 
 mapview(deso)
+
+# Save the spatial dataset
+st_write(deso, "G:/Samhällsanalys/GIS/projekt/GIS-praktik/Waleed_Liv/Praktik_2023/deso_dalarna.gpkg")
+
